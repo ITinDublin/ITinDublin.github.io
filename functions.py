1000bdd6649f3c694ea8628fb7bb1cdf596b0128
Lucas <lffsantos@gmail.com>
2016-09-28 04:58:54 -0300

    fix encode

--- functions.py
+++ functions.py
@@ -12,7 +12,7 @@ def GET_WIKI_PAGES():
     content = 'content/pages/wiki'
     wiki_pages = []
     for page in os.listdir(content):
-        ler_arquivo = open(content+'/'+page, 'r')
+        ler_arquivo = open(content+'/'+page, 'r', encoding='utf-8')
         titulo = ler_arquivo.readline()
         extensao = page.split(".")[1]
         url = page.split(".")[0]
