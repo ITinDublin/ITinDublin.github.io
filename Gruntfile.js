84c99c2a0edee4286f82a265b984528ae3a705ee
Douglas Deodato <douglasdeodato@gmail.com>
2016-09-30 12:54:15 +0100

    config for grunt and npm

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+  var config = {};
+
+  //src ===============================
+  var src;
+  config.src = src = {
+    sassMain: 'themes/malt/scss/main.scss',
+    distFolder: 'themes/malt/public/stylesheets/it-in-dublin.dist.css',
+    devFolder: 'themes/malt/public/stylesheets/it-in-dublin.dev.css',
+    sassFolder: 'themes/malt/scss/**/*.scss',
+    serverPort: 8000
+  };
+
+  //Watch ===============================
+  config.watch = {
+    scripts: {
+      files: ["<%= src.sassFolder %>"],
+      tasks: ["sass:dist"]
+    }
+  }
+
+  //Sass ===============================
+  var sass;
+  config.sass = sass = {};
+
+  //sass distribution
+  sass.dist = {
+    options: {
+      style: "compressed",
+      noCache: true,
+      sourcemap: 'none',
+      update: true
+    },
+    files: {
+      "<%= src.distFolder %>": "<%= src.sassMain %>"
+    }
+  };
+
+  //sass development env.
+  sass.dev = {
+    options: {
+      style: "expanded",
+      lineNumber: true,
+    },
+    files: {
+      "<%= src.devFolder %>": "<%= src.sassMain %>"
+    }
+  };
+
+  //grunt serve ===============================
+  config.connect = {
+    server: {
+      options: {
+        livereload: true,
+        port: "<%= src.serverPort %>"
+      }
+    }
+  };
+
+  //Register custom tasks ===============================
+  grunt.registerTask('default', ['dev']);
+  grunt.registerTask('dev', ['sass:dev']);
+  grunt.registerTask('dist', ['sass:dist']);
+  grunt.registerTask('serve', ['connect:server', 'watch']);
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies'
+  });
+
+  //General setup ===============================
+  grunt.initConfig(config);
+
+};
